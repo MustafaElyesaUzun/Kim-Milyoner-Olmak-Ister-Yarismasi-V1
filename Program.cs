d9e09ee5b0f94bf1d39eca59982121d510551965
EmirOcalan <emirocalan58@gmail.com>
2023-03-28 17:52:35 +0300

    feat: add do you know the rules

--- Program.cs
+++ Program.cs
@@ -16,6 +16,20 @@
             string telefonJoker2 = Console.ReadLine();
             Console.Write("3.Telefon Jokerinizi Giriniz: ");
             string telefonJoker3 = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Hoşgeldiniz " + isim + " " + soyisim);
+
+            Console.Write("Kuralları Biliyor Musunuz? : ");
+            char kuralCevap = char.Parse(Console.ReadLine().ToUpper());
+
+            if (kuralCevap != 'E')
+            {
+                Console.WriteLine("Kurallar: Yarışmada toplam 12 soru yer almaktadır. " +
+                    "2. soruda 2.000 TL, 7. soruda ise 30.000 TL'lik baraj soruları vardır. " +
+                    "Yarışmayı kaybettiğiniz takdirde en son geçtiğiniz baraj sorusunun ödülü sizin olur. " +
+                    "Çekildiğiniz takdirde en son doğru yanıtladığınız sorunun ödülünü alırsınız");
+            }
         }
     }
 }
